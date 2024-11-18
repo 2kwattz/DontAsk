b1b4d8d26b3f4665f947cb59a09f42dbaa854e61
Roshan Bhatia <prakashbhatia1970@gmail.com>
2024-11-18 21:29:58 +0530

    Frontend UI Completed

--- src/server.js
+++ src/server.js
@@ -4,17 +4,23 @@ const path = require('path');
 const port = 80; 
 
 // Paths
-
 const staticPath = path.join(__dirname, "../public");
+
+console.log("StaticPath ",staticPath)
 
 // Middlewares
 app.use(express.static(staticPath));
 
+// View Engine
+app.set('view engine','hbs')
 
+// Routes
 
+app.get("/", function(request,response){
+    response.render("index")
+})
 
 // App Listen
-
 app.listen(port, function(){
     console.log(`Node Server is running, listening on port ${port} `)
 })
